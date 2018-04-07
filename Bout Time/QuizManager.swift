c4ce113207b849732f278a6c462d1fae861371ff
Jaakko Kenttä <kenttajaa@gmail.com>
2018-04-07 19:26:49 +0300

    Added button controls and quiz manager class. Code is pretty ugly but it works :)

--- Bout Time/QuizManager.swift
+++ Bout Time/QuizManager.swift
@@ -7,14 +7,58 @@
 //
 
 import Foundation
-
-
+import UIKit
+import GameplayKit
 
 class QuizManager {
-    let events: [Event]
+    var events: [Event]
+    var firstText: UITextView
+    var secondText: UITextView
+    var thirdText: UITextView
+    var fourthText: UITextView
+    var eventIndex: Int = 0
+    var event1: Event?
+    var event2: Event?
+    var event3: Event?
+    var event4: Event?
     
-    
-    init(events: [Event]){
+    init(events: [Event], firstText: UITextView, secondText: UITextView, thirdText: UITextView, fourthText: UITextView){
         self.events = events
+        self.firstText = firstText
+        self.secondText = secondText
+        self.thirdText = thirdText
+        self.fourthText = fourthText
+    }
+    
+    ///shuffle events array
+    func shuffleEvents(){
+         events = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: self.events) as! [Event]
+    }
+    
+    func checkOrder() {
+        
+    }
+    
+    func startGame() {
+        shuffleEvents()
+        startRound()
+    }
+    
+    func startRound() {
+        //add new texts on game. Raise the index number for the next round
+        event1 = events[eventIndex]
+        event2 = events[eventIndex + 1]
+        event3 = events[eventIndex + 2]
+        event4 = events[eventIndex + 3]
+        eventIndex += 4
+        
+        updateTexts()
+    }
+
+    func updateTexts() {
+        firstText.text = event1?.eventName
+        secondText.text = event2?.eventName
+        thirdText.text = event3?.eventName
+        fourthText.text = event4?.eventName
     }
 }
--- Bout Time/ViewController.swift
+++ Bout Time/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var gameObject: QuizManager
+    let events: [Event]
+    var gameObject: QuizManager!
     
     @IBOutlet weak var firstText: UITextView!
     @IBOutlet weak var secondText: UITextView!
@@ -24,7 +25,7 @@ class ViewController: UIViewController {
         do{
             let dictionary = try PlistConverter.dictionary(fromFile: "events", ofType: "plist")
             let eventsArray = try EventsUnarchiver.eventsSelection(fromDictionary: dictionary)
-            gameObject = QuizManager(events: eventsArray)
+            events = eventsArray
         } catch let error {
             fatalError("\(error)")
         }
@@ -33,14 +34,43 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(gameObject.events[0].site)
+        //print(gameObject.events[0].site)
+        gameObject = QuizManager(events: events, firstText: firstText, secondText: secondText, thirdText: thirdText, fourthText: fourthText)
+        
+        gameObject.startGame()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    @IBAction func orderChange(_ sender: UIButton){
+        let button = sender
+        
+        //take tempEvent
+        var event : Event
+        
+        //change event places depending which button was pressed
+        switch button.tag {
+        case 1, 2:
+            event = gameObject.event2!
+            gameObject.event2 = gameObject.event1
+            gameObject.event1 = event
+        case 3, 4:
+            event = gameObject.event3!
+            gameObject.event3 = gameObject.event2
+            gameObject.event2 = event
+        case 5,6:
+            event = gameObject.event4!
+            gameObject.event4 = gameObject.event3
+            gameObject.event3 = event
+        default:
+            print("Something is wrong")
+        }
+        gameObject.updateTexts()
+    }
 
 }
 
