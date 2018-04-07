e297971cd1c2492177c97c6cf9c12b980d8cd62d
Jaakko Kenttä <kenttajaa@gmail.com>
2018-04-07 11:42:31 +0300

    PlistConverter class.

---
+++ Bout Time/EventsProvider.swift
@@ -0,0 +1,81 @@
+//
+//  EventsProvider.swift
+//  Bout Time
+//
+//  Created by Jaakko Kenttä on 07/04/2018.
+//  Copyright © 2018 Jaakko Kenttä. All rights reserved.
+//
+
+import Foundation
+
+
+protocol EventData {
+    var eventName: String { get }
+    var time: Int { get }
+    var site: String { get }
+}
+
+enum EventsConverterError : Error {
+    case invalidResource
+    case conversionFailure
+}
+
+
+struct Event :EventData{
+    let eventName: String
+    var time: Int
+    var site: String
+    
+    init(time :Int, site: String, eventName: String){
+        self.eventName = eventName
+        self.time = time
+        self.site = site
+    }
+}
+
+struct EventSelection {
+    var selection: [Event]
+}
+
+class PlistConverter {
+    
+    static func dictionary(fromFile name :String, ofType type: String) throws -> [String: AnyObject]{
+        guard let path = Bundle.main.path(forResource: name, ofType: type) else {
+            throw EventsConverterError.invalidResource
+        }
+        
+        guard let dictionary = NSDictionary(contentsOfFile: path) as? [String: AnyObject] else {
+            throw EventsConverterError.conversionFailure
+        }
+        
+        return dictionary
+    }
+}
+
+class EventsUnarchiver {
+    static func eventsSelection(fromDictionary dictionary : [String: AnyObject]) throws -> [Event] {
+        
+        var events: [Event] = []
+        for (key, value) in dictionary {
+            if let dictionary = value as? [String: AnyObject], let time = dictionary["time"] as? Int, let site = dictionary["site"] as? String {
+                let newEvent = Event(time: time, site: site, eventName: key)
+                events.append(newEvent)
+            }
+        }
+        return events
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Bout Time/ViewController.swift
+++ Bout Time/ViewController.swift
@@ -13,6 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        do{
+            let dictionary = try PlistConverter.dictionary(fromFile: "events", ofType: "plist")
+            
+            let eventsArray = try EventsUnarchiver.eventsSelection(fromDictionary: dictionary)
+            
+            for event in eventsArray {
+                print(event.time)
+            }
+        } catch let error {
+            fatalError("\(error)")
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
